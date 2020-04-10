2ae01a414cf8edb9f4375448799b0f6c7f5e7c17
JoeScripter <BlavikenCZ@gmail.com>
2020-04-10 02:33:05 +0200

    responsive slider

--- UI/src/sample/Controller.java
+++ UI/src/sample/Controller.java
@@ -1,21 +1,27 @@
 package sample;
 
+import com.jfoenix.controls.JFXSlider;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 public class Controller {
+
+
 
     public void changeScreenKitchen(ActionEvent evt) throws IOException {
         Parent kitchenViewParent = FXMLLoader.load(getClass().getResource("KitchenWindow.fxml"));
@@ -52,5 +58,20 @@ public class Controller {
 
         window.setScene(bedroomView);
         window.show();
+    }
+    @FXML
+    private JFXSlider slider;
+    @FXML
+    private void initialize(){
+        temp_c.setText(new DecimalFormat("#.00").format(slider.getValue()));
+    }
+
+    @FXML
+    private Label temp_c ;
+
+    @FXML
+    private void increment() {
+        String text = String.valueOf(new DecimalFormat("#.00").format(slider.getValue()));
+        temp_c.setText(text);
     }
 }
